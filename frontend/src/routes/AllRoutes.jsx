abbb70e7f08277e6e21c8629bc804ffbac7cc33e
JOEL K JAMES <85893912+Joel-K-James@users.noreply.github.com>
2024-04-28 03:02:17 +0530

    Add files via upload

---
+++ frontend/src/routes/AllRoutes.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Route,Routes} from "react-router-dom"
+import { Home } from '../pages/Home'
+import { Login } from '../pages/Login'
+import { Signup } from '../pages/Signup'
+import { UserAuthContextProvider} from '../context/Authcontext'
+import Verify from '../pages/Verify'
+
+
+export const AllRoutes = () => {
+  return (
+    <UserAuthContextProvider>
+    <Routes>
+       <Route path='/' element={<Login/>}/>
+       <Route path="/home" element={<Home/>}/>
+       <Route path='/signup' element={<Signup/>}/>
+       <Route path='/verify' element={<Verify/>}/>
+    </Routes>
+    </UserAuthContextProvider>
+  )
+}
+
